ec1b5e8e5c8d55de3789c0115f57ba19f2453061
Vic <vic@Vics-iMac.local>
2026-01-02 03:25:07 -0700

    PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'neural-architect-v1.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
